4dcc156d37f1f4202e4134eff4ad63629375ba28
Hedi Ben Mustapha <hedi.benmustapha@msb.tn>
2019-08-05 18:58:18 +0100

    commit first analysis

---
+++ preliminary_weighted_analysis.R
@@ -0,0 +1,28 @@
+data <- load_data(file = "./input/data.csv")
+sampling_frame <- load_samplingframe(file = "./input/sampling_frame.csv")
+questionnaire <- load_questionnaire(data = "./input/data.csv",
+                                    questions = "./input/survey.csv",
+                                    choices = "./input/choices.csv",
+                                    choices.label.column.to.use = "label::English (en)")
+analysisplan <- load_analysisplan(file = "./input/analysisplan.csv")
+
+
+kobostandards::check_input(data = data, questions = questionnaire, choices = "./input/choices.csv" ,samplingframe = sampling_frame,
+                           analysisplan = analysisplan) %>% write.csv("check_input.csv")
+
+
+
+weights <-map_to_weighting(sampling.frame = sampling_frame,
+                           data.stratum.column = "strata.names",
+                           sampling.frame.population.column = "population",
+                           sampling.frame.stratum.column = "strata.names",
+                           data = data)
+
+
+weighted_results <- from_analysisplan_map_to_output(data = data,
+                                                    analysisplan = analysisplan,
+                                                    weighting = weights,
+                                                    questionnaire = questionnaire)
+
+weighted_results_labeled <- lapply(weighted_results$results, map_to_labeled,questionnaire)
+map_to_master_table(results_object = weighted_results_labeled, filename = "./output/bio_food_shelter.csv")
