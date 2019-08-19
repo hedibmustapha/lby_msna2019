b1b5f64d3723b91a54fdacfad830889c08567323
Hedi Ben Mustapha <hedi.benmustapha@msb.tn>
2019-08-19 15:43:47 +0100

    update all inputs and create allvar. analysis plan

--- preliminary_weighted_analysis.R
+++ preliminary_weighted_analysis.R
@@ -3,6 +3,7 @@ rm(list = ls())
 library(hypegrammaR)
 library(parallel)
 
+source("analysisplan_factory.R")
 questions <- read.csv("./input/survey.csv", stringsAsFactors = F)
 choices <- read.csv("./input/choices.csv", stringsAsFactors = F)
 data <- load_data(file = "./input/data.csv")
@@ -11,8 +12,10 @@ questionnaire <- load_questionnaire(data = data,
                                     questions = questions,
                                     choices = choices,
                                     choices.label.column.to.use = "label::English (en)")
-analysisplan <- load_analysisplan(file = "./input/analysisplan.csv")
-
+#analysisplan <- load_analysisplan(file = "./input/analysisplan.csv")
+analysisplan <- make_analysisplan_all_vars(df = data,questionnaire = questionnaire,repeat.for.variable = "mantika_label")
+write.csv(analysisplan,"./input/analysisplan.csv")
+analysisplan <-read.csv("./input/analysisplan.csv",stringsAsFactors = F)
 
 kobostandards::check_input(data = data, questions = questions, choices = choices ,samplingframe = sampling_frame,
                            analysisplan = analysisplan) %>% write.csv("check_input.csv")
@@ -25,11 +28,11 @@ weights <-map_to_weighting(sampling.frame = sampling_frame,
                            sampling.frame.stratum.column = "strata.names",
                            data = data)
 
-
+analysisplan <-read.csv("./input/analysisplan.csv",stringsAsFactors = F)
 weighted_results <- from_analysisplan_map_to_output(data = data,
                                                     analysisplan = analysisplan,
                                                     weighting = weights,
                                                     questionnaire = questionnaire)
 
 weighted_results_labeled <- lapply(weighted_results$results, map_to_labeled,questionnaire)
-map_to_master_table(results_object = weighted_results_labeled, filename = "./output/bio_food_shelter.csv")
+map_to_master_table(results_object = weighted_results_labeled, filename = "./output/overall_byMantika.csv")
